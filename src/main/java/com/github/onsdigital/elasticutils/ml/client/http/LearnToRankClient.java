fd5cf4ead2d46f88d62db663e5a3a325d9f6830b
David Sullivan <dsullivan0190@gmail.com>
2017-12-05 16:28:05 +0000

    Added RankLib jars

--- src/main/java/com/github/onsdigital/elasticutils/ml/client/http/LearnToRankClient.java
+++ src/main/java/com/github/onsdigital/elasticutils/ml/client/http/LearnToRankClient.java
@@ -156,6 +156,8 @@ public class LearnToRankClient implements AutoCloseable {
         return SltrResponse.fromResponse(response);
     }
 
+    // RANKLIB //
+
     @Override
     public void close() throws Exception {
         if (LOGGER.isDebugEnabled()) LOGGER.debug("Closing RestClient");
