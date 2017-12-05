d7c04876dff238cefebb5e412aea898d009988a6
David Sullivan <dsullivan0190@gmail.com>
2017-12-05 17:48:55 +0000

    Working model upload

--- src/main/java/com/github/onsdigital/elasticutils/ml/ranklib/models/RankLibModel.java
+++ src/main/java/com/github/onsdigital/elasticutils/ml/ranklib/models/RankLibModel.java
@@ -1,6 +1,13 @@
 package com.github.onsdigital.elasticutils.ml.ranklib.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.github.onsdigital.elasticutils.ml.client.http.LearnToRankClient;
+import com.github.onsdigital.elasticutils.ml.util.LearnToRankHelper;
+import org.apache.http.util.EntityUtils;
+import org.elasticsearch.client.Response;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * @author sullid (David Sullivan) on 05/12/2017
@@ -11,9 +18,13 @@ public class RankLibModel {
     private String name;
     private Model model;
 
-    public RankLibModel(String name, String type, String definition) {
+    public RankLibModel(String name, String definition) {
         this.name = name;
-        this.model = new Model(type, definition);
+        this.model = new Model(definition);
+    }
+
+    private RankLibModel() {
+        // For Jenkins
     }
 
     public String getName() {
@@ -24,13 +35,45 @@ public class RankLibModel {
         return model;
     }
 
+    public static String getDefinition(String filename) throws IOException {
+        String content = new String(Files.readAllBytes(Paths.get(filename)));
+        return content;
+    }
+
     private class Model {
-        private String type;
+        private final String type = "model/ranklib";
         private String definition;
 
-        public Model(String type, String definition) {
-            this.type = type;
+        public Model(String definition) {
             this.definition = definition;
+        }
+
+        private Model() {
+            // For Jenkins
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public String getDefinition() {
+            return definition;
+        }
+    }
+
+    public static void main(String[] args) {
+        try (LearnToRankClient client = LearnToRankHelper.getLTRClient("localhost")) {
+            String filename = "/Users/sullid/idea/elasticsearch-learning-to-rank/demo/model_java.txt";
+            RankLibModel model = new RankLibModel("test_6", RankLibModel.getDefinition(filename));
+
+            String featureset = "movie_features";
+
+            Response response = client.uploadModel(featureset, model);
+            String entity = EntityUtils.toString(response.getEntity());
+
+            System.out.println(entity);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
