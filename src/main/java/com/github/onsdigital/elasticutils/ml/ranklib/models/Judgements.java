78b3de0ca54c150a57bca5ef49bf3fe91df0d7d7
David Sullivan <dsullivan0190@gmail.com>
2017-12-14 09:10:41 +0000

    Added Judgements class which computes NDCG metric for a list of Judgement objects and corresponding test class

---
+++ src/main/java/com/github/onsdigital/elasticutils/ml/ranklib/models/Judgements.java
@@ -0,0 +1,110 @@
+package com.github.onsdigital.elasticutils.ml.ranklib.models;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author sullid (David Sullivan) on 14/12/2017
+ * @project dp-elasticutils-ltr
+ *
+ * Computes metrics to determine search performance
+ */
+public class Judgements {
+
+    private static final float MAX_SCORE = 4.0f;
+    private static final float MIN_SCORE = 0.0f;
+
+    private int queryId;
+    private List<Judgement> judgementList;
+
+    public Judgements(int queryId, List<Judgement> judgements) {
+        this.queryId = queryId;
+
+        // Ensure all judgements are for this queryId
+        for (Judgement judgement : judgements) {
+            if (judgement.getQueryId() != this.queryId) {
+                throw new RuntimeException(String.format("Invalid judgement for queryID: %d : %s", this.queryId, judgement));
+            }
+        }
+        this.judgementList = judgements;
+    }
+
+    public float[] cumulativeGain() {
+        float[] cumulativeGain = new float[this.judgementList.size()];
+
+        float total = 0.0f;
+        for (int i = 0; i < cumulativeGain.length; i++) {
+            total += this.judgementList.get(i).getJudgement();
+            cumulativeGain[i] = total;
+        }
+        return cumulativeGain;
+    }
+
+    public float[] discountedCumulativeGain() {
+        float[] discountedCumulativeGain = new float[this.judgementList.size()];
+
+        float total = 0.0f;
+        for (int i = 0; i < discountedCumulativeGain.length; i++) {
+            Judgement judgement = this.judgementList.get(i);
+            total += judgement.getJudgement() / Float.valueOf(judgement.getRank());
+            discountedCumulativeGain[i] = total;
+        }
+        return discountedCumulativeGain;
+    }
+
+    /**
+     * Computes the main Normalised Discounted Cumulative Gain (NDGC) metric of performance
+     * @return
+     */
+    public float[] normalisedDiscountedCumulativeGain() {
+        float[] normalisedDiscountedCumulativeGain = new float[this.judgementList.size()];
+
+        float[] discountedCumulativeGain = this.discountedCumulativeGain();
+        float[] idealDiscountedCumulativeGain = idealDiscountedCumulativeGain(normalisedDiscountedCumulativeGain.length);
+
+        for (int i = 0; i < normalisedDiscountedCumulativeGain.length; i++) {
+            normalisedDiscountedCumulativeGain[i] = discountedCumulativeGain[i] / idealDiscountedCumulativeGain[i];
+        }
+
+        return normalisedDiscountedCumulativeGain;
+    }
+
+    /**
+     *
+     * @param num
+     * @return
+     *
+     * Generates evenly spaced ideal judgements between MAX_SCORE and 0
+     */
+    public static float[] idealJudgement(int num) {
+        float[] idealJudgement = new float[num];
+
+        int i = 0;
+        float increment = (1.0f / ((float) num - 1)) * num;
+        for (float val = idealJudgement.length; val > 0; val-=increment) {
+            idealJudgement[i] = (val / (float) num) * MAX_SCORE;
+            i++;
+        }
+        return idealJudgement;
+    }
+
+    public static float[] idealDiscountedCumulativeGain(int num) {
+        float[] idealGain = idealJudgement(num);
+        float[] idealDiscountedCumulativeGain = new float[num];
+
+        float total = 0.0f;
+        for (int i = 0; i < idealGain.length; i++) {
+            total += idealGain[i] / (float) (i+1);  // Compute idealDiscountedGain
+            idealDiscountedCumulativeGain[i] = total; // Compute cumulativeIdealDiscountedGain
+        }
+        return idealDiscountedCumulativeGain;
+    }
+
+    public static void main(String[] args) {
+        int num = 4;
+        float[] idealJudgements = Judgements.idealJudgement(num);
+
+        System.out.println(Arrays.toString(idealJudgements));
+    }
+
+}
---
+++ src/test/java/com/github/onsdigital/elasticutils/ml/TestNDCG.java
@@ -0,0 +1,42 @@
+package com.github.onsdigital.elasticutils.ml;
+
+import com.github.onsdigital.elasticutils.ml.ranklib.models.Judgement;
+import com.github.onsdigital.elasticutils.ml.ranklib.models.Judgements;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import static junit.framework.TestCase.assertTrue;
+
+/**
+ * @author sullid (David Sullivan) on 14/12/2017
+ * @project dp-elasticutils-ltr
+ */
+public class TestNDCG {
+
+    private static final List<Judgement> judgementList;
+    private static final float[] expected;
+    private static final int QUERY_ID = 1;
+
+    static {
+        judgementList = new LinkedList<Judgement>() {{
+           add(new Judgement(2.0f, QUERY_ID, 1));
+           add(new Judgement(0.0f, QUERY_ID, 2));
+           add(new Judgement(3.0f, QUERY_ID, 3));
+           add(new Judgement(2.0f, QUERY_ID, 4));
+        }};
+
+        expected = new float[]{0.5f, 0.37500003f, 0.51923084f, 0.6057693f};
+    }
+
+    @Test
+    public void testNDCG() {
+        Judgements judgements = new Judgements(QUERY_ID, judgementList);
+
+        float[] ndcg = judgements.normalisedDiscountedCumulativeGain();
+        assertTrue(Arrays.equals(ndcg, expected));
+    }
+
+}
